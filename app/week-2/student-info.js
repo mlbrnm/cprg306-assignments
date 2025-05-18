62fddce7526dfb797eae1045048919f4e859e93c
Melbourne Marsden <114309689+mlbrnm@users.noreply.github.com>
2025-05-18 15:23:56 -0600

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Melbourne Marsden</p>
+      <Link href="https://github.com/mlbrnm">My GitHub</Link>
+    </div>
+  );
+}
